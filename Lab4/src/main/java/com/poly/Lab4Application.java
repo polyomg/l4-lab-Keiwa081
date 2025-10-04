245cde86092c1226f0b05cd804273a242f433619
Keiwa081 <anminecraft134@gmail.com>
2025-10-04 14:33:57 +0700

    TS01299 - Nguyen Thai Thien An - Lab 4

--- Lab4/src/main/java/com/poly/Lab4Application.java
+++ Lab4/src/main/java/com/poly/Lab4Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ts01294Lab4Application {
+public class Lab4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ts01294Lab4Application.class, args);
+		SpringApplication.run(Lab4Application.class, args);
 	}
 
 }
