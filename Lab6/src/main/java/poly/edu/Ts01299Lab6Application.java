8109b2796fd6179c0c8966a8416dd19a5f0145dc
Keiwa081 <anminecraft134@gmail.com>
2025-10-16 14:18:05 +0700

    TS01299 - Nguyen Thai Thien An - Lab 6

---
+++ Lab6/src/main/java/poly/edu/Ts01299Lab6Application.java
@@ -0,0 +1,13 @@
+package poly.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ts01299Lab6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ts01299Lab6Application.class, args);
+	}
+
+}
